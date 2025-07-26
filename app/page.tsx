6e62034d3246771f51641a402fabed884a3990bb
fadli154 <fadlihifziansyah154@gmail.com>
2025-07-27 06:56:37 +0700

    Fi: suspens

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 // app/page.tsx
+import { Suspense } from "react";
 import HeroWrapper from "@/app/partials/hero-section/HeroWrapper";
 
 export default function HomePage() {
-  return <HeroWrapper />;
+  return (
+    <main>
+      <Suspense fallback={<div>Loading...</div>}>
+        <HeroWrapper />
+      </Suspense>
+    </main>
+  );
 }
