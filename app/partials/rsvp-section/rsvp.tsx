c8594052f42b5bbb1f3f4beba85be4f7036a2ee4
fadli154 <fadlihifziansyah154@gmail.com>
2025-07-29 05:45:04 +0700

    add: un finished rsvp

--- app/partials/rsvp-section/rsvp.tsx
+++ app/partials/rsvp-section/rsvp.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 export default function RSVPSection() {
   return (
-    <section id="rsvp" className="py-20 px-4 sm:px-8 lg:px-24 bg-[url('/img/floral-pattern.jpg')] text-foreground bg-cover bg-center bg-no-repeat dark:opacity-5">
+    <section id="rsvp" className="py-20 px-4 sm:px-8 lg:px-24 text-foreground">
       <div className="max-w-2xl mx-auto text-center">
         <motion.h2 initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.6 }} className="text-3xl font-bold mb-4 dark:text-white2-500">
           RSVP
