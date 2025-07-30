c90308aded6b87a8829aaa365366852e339e84b9
fadli154 <fadlihifziansyah154@gmail.com>
2025-07-30 22:42:03 +0700

    Add: animated card

--- app/partials/info-section/info.tsx
+++ app/partials/info-section/info.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import { FaClock } from "react-icons/fa";
 import { motion } from "framer-motion";
-import { Grey_Qo } from "next/font/google";
+import { Sacramento } from "next/font/google";
 import clsx from "clsx";
+import AnimatedCard from "@/app/elements/card/AnimatedCard";
 
-const greGrey_Qo = Grey_Qo({ subsets: ["latin"], weight: "400" });
+const greSacramento = Sacramento({ subsets: ["latin"], weight: "400" });
 
 export default function EventInfo() {
   return (
     <section className="w-full bg-primary-500/10 dark:bg-dark2-600 text-white dark:text-white2-500 px-6 py-20 transition-colors duration-500" id="info">
       <div className="max-w-5xl mx-auto text-center">
-        <motion.h2 initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.6 }} className={clsx(greGrey_Qo.className, "text-text-500 text-6xl mb-4")}>
+        <motion.h2 initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.6 }} className={clsx(greSacramento.className, "text-text-500 text-6xl mb-4")}>
           Informasi Acara
         </motion.h2>
 
@@ -25,42 +25,21 @@ export default function EventInfo() {
             width="100%"
             height="100%"
             loading="lazy"
-            className="rounded-xl shadow-lg dark:shadow-primary-500/15 border-2 border-white/80"
+            className="rounded-xl shadow-lg dark:shadow-primary-500/15 border-2 border-white/10"
             allowFullScreen
           ></iframe>
         </motion.div>
 
-        <motion.p initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.5, duration: 0.5 }} className="text-sm mb-10 text-dark2-600/40 dark:text-white2-500/80">
+        <motion.p initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.5, duration: 0.5 }} className="text-sm mt-10 text-dark2-600/40 dark:text-white2-500/80">
           Diharapkan untuk tidak salah alamat dan tanggal. Jika tiba di tempat tanpa tanda-tanda pernikahan, cek ulang jadwal dan lokasi.
         </motion.p>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <motion.div
-            initial={{ opacity: 0, y: 30 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0.6, duration: 0.5 }}
-            className="bg-secondary-500/10 dark:bg-white2-500/10 p-6 backdrop-blur-xl rounded-xl shadow-lg dark:shadow-primary-500/15 py-8"
-          >
-            <h3 className="text-2xl font-bold mb-2 text-text-500/80">Akad Nikah</h3>
-            <div className="flex items-center gap-2 justify-center text-dark2-600/50 dark:text-white2-500 mb-1">
-              <FaClock /> <span className="text-center text-dark2-600/50 dark:text-white2-500">08.00 - 10.00</span>
-            </div>
-            <p className="text-dark2-600/50 dark:text-white2-500 mb-2">Minggu, 20 November 2023</p>
-            <p className="text-sm text-dark2-600/50 dark:text-white2-500/70">Saat acara akad diharapkan untuk menjaga kekhidmatan dan hadir tepat waktu.</p>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+          <motion.div initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.6, duration: 0.5 }}>
+            <AnimatedCard title="Akad Nikah" date="Minggu, 07 September 2025" time="Pukul 11.00 WIB" />
           </motion.div>
-
-          <motion.div
-            initial={{ opacity: 0, y: 30 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0.7, duration: 0.5 }}
-            className="bg-secondary-500/10 dark:bg-white2-500/10 p-6 backdrop-blur-xl rounded-xl shadow-lg dark:shadow-primary-500/15 py-8"
-          >
-            <h3 className="text-2xl font-bold mb-2 text-text-500/80">Resepsi</h3>
-            <div className="flex items-center gap-2 justify-center text-dark2-600/50 dark:text-white2-500 mb-1">
-              <FaClock /> <span className="text-center">11.00 - selesai</span>
-            </div>
-            <p className="text-dark2-600/50 dark:text-white2-500 mb-2">Minggu, 20 November 2023</p>
-            <p className="text-sm text-dark2-600/50 dark:text-white2-500/70">Saat acara resepsi diharapkan untuk tetap tertib dan mengikuti alur yang telah ditentukan.</p>
+          <motion.div initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.8, duration: 0.7 }}>
+            <AnimatedCard title="Resepsi Nikah" date="Minggu, 07 September 2025" time="Pukul 12.00 WIB" />
           </motion.div>
         </div>
       </div>
