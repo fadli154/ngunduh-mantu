6d219d819ffcb0dadede754fc7edf0e81dd49b28
fadli154 <fadlihifziansyah154@gmail.com>
2025-07-30 22:48:43 +0700

    Fix: layout info section

--- app/elements/card/AnimatedCard.tsx
+++ app/elements/card/AnimatedCard.tsx
@@ -11,14 +11,14 @@ type CardProps = {
 
 export default function AnimatedCard({ title, date, time }: CardProps) {
   return (
-    <div className="e-card playing relative backdrop-blur-3xl w-full h-70 rounded-2xl overflow-hidden shadow-lg dark:shadow-white2-500/20 bg-transparent mx-auto mt-5">
+    <div className="e-card playing relative backdrop-blur-3xl w-full h-65 rounded-2xl overflow-hidden shadow-lg dark:shadow-white2-500/20 bg-transparent mx-auto">
       {/* Animated Waves */}
       <motion.div className="absolute w-[540px] h-[700px] opacity-60 left-0 top-0 -ml-[50%] -mt-[70%] bg-gradient-to-tr from-[#af40ff] via-[#5b42f3] to-[#00ddeb] rounded-[40%] animate-wave1 wave" />
       <motion.div className="absolute w-[540px] h-[700px] opacity-60 left-0 top-0 -ml-[50%] -mt-[70%] bg-gradient-to-tr from-[#af40ff] via-[#5b42f3] to-[#00ddeb] rounded-[40%] animate-wave2 wave" />
       <motion.div className="absolute w-[540px] h-[700px] opacity-60 left-0 top-0 -ml-[50%] -mt-[70%] bg-gradient-to-tr from-[#af40ff] via-[#5b42f3] to-[#00ddeb] rounded-[40%] animate-wave3 wave" />
 
       {/* Info Section */}
-      <div className="relative flex justify-center items-center flex-col right-0 top-20 text-center text-white font-semibold text-lg">
+      <div className="relative flex justify-center items-center flex-col right-0 top-16 text-center text-white font-semibold text-lg">
         <FaClock className="w-10 h-10 mb-1" />
         <h3 className="text-2xl font-bold text-white2-500/80 dark:text-white2-500/40">{title}</h3>
         <div className="flex items-center gap-2 justify-center text-dark2-600/50 dark:text-white2-500 mb-1">
--- app/partials/info-section/info.tsx
+++ app/partials/info-section/info.tsx
@@ -11,9 +11,15 @@ export default function EventInfo() {
   return (
     <section className="w-full bg-primary-500/10 dark:bg-dark2-600 text-white dark:text-white2-500 px-6 py-20 transition-colors duration-500" id="info">
       <div className="max-w-5xl mx-auto text-center">
-        <motion.h2 initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.6 }} className={clsx(greSacramento.className, "text-text-500 text-6xl mb-4")}>
-          Informasi Acara
-        </motion.h2>
+        <div className="flex flex-col items-center mb-8">
+          {/* Hiasan garis di atas */}
+          <div className="w-18 h-1 bg-text-500 rounded-full mb-2"></div>
+
+          {/* Judul */}
+          <motion.h2 initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.6 }} className={clsx(greSacramento.className, "text-text-500 text-6xl mb-4 text-center")}>
+            Informasi <span className="text-dark2-600 dark:text-white2-500">Acara</span>
+          </motion.h2>
+        </div>
 
         <motion.p initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.2, duration: 0.5 }} className="text-sm md:text-md mb-6 text-dark2-600/60 dark:text-white2-500/90">
           <strong>Alamat:</strong> Perumahan Total Persada, Jl. Maos Blok H9 No. 34 RT 003/ RW 008 <br /> Kel. Gembor, Kec. Periuk, Kota Tangerang
@@ -34,7 +40,7 @@ export default function EventInfo() {
           Diharapkan untuk tidak salah alamat dan tanggal. Jika tiba di tempat tanpa tanda-tanda pernikahan, cek ulang jadwal dan lokasi.
         </motion.p>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
           <motion.div initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.6, duration: 0.5 }}>
             <AnimatedCard title="Akad Nikah" date="Minggu, 07 September 2025" time="Pukul 11.00 WIB" />
           </motion.div>
